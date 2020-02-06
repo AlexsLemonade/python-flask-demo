ae90d72ddf86669955937dbfa073f5cafa29cd96
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-02-06 13:37:32 -0500

    Add validation to User routes.

--- resources_portal/views/user.py
+++ resources_portal/views/user.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, jsonify, request
+from flask_request_validator import JSON, PATH, MaxLength, MinLength, Param, validate_params
 from resources_portal.db import db
 from resources_portal.models import User
 
 user = Blueprint("user", __name__)
 
+str_one_to_hundred_validator = [MinLength(1), MaxLength(100)]
+
+user_validator = [
+    Param("first_name", JSON, str, rules=str_one_to_hundred_validator),
+    Param("last_name", JSON, str, rules=str_one_to_hundred_validator),
+    Param("orcid", JSON, str, rules=str_one_to_hundred_validator),
+    Param("email_address", JSON, str, rules=str_one_to_hundred_validator),
+]
+
 
 @user.route("/", methods=["POST"])
 @user.route("", methods=["POST"])
-def create():
+@validate_params(*user_validator)
+def create(*args):
     db.session.add(User(**request.get_json()))
     db.session.commit()
 
@@ -16,7 +27,8 @@ def create():
 
 @user.route("/<user_id>/", methods=["PUT"])
 @user.route("/<user_id>", methods=["PUT"])
-def update(user_id: int):
+@validate_params(Param("user_id", PATH, int), *user_validator)
+def update(user_id: int, *args):
     # Make sure the User exists
     db.session.query(User).get_or_404(user_id)
 
@@ -28,6 +40,7 @@ def update(user_id: int):
 
 @user.route("/<user_id>/", methods=["GET"])
 @user.route("/<user_id>", methods=["GET"])
+@validate_params(Param("user_id", PATH, int))
 def show(user_id: int):
     user = db.session.query(User).get_or_404(user_id)
 
